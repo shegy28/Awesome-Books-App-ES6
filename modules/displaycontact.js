4e84c8a3db58b2ede9d74a55be3fe854f2b68910
shegy28 <shegy28@gmail.com>
2022-12-05 19:17:29 +0200

    Add export default to function

--- modules/displaycontact.js
+++ modules/displaycontact.js
@@ -1,7 +1,7 @@
-  const displayContact = () => {
-    document.getElementById('contact').classList.add('block');
-    document.getElementById('Book-list').classList.remove('block');
-    document.getElementById('Add-new').classList.remove('block');
-  }
+const displayContact = () => {
+  document.getElementById('contact').classList.add('block');
+  document.getElementById('Book-list').classList.remove('block');
+  document.getElementById('Add-new').classList.remove('block');
+};
 
-  export {displayContact}
+export default displayContact;
--- modules/displaynew.js
+++ modules/displaynew.js
@@ -1,7 +1,7 @@
 const displayNew = () => {
   document.getElementById('Add-new').classList.add('block');
-    document.getElementById('Book-list').classList.remove('block');
-    document.getElementById('contact').classList.remove('block');
-}
+  document.getElementById('Book-list').classList.remove('block');
+  document.getElementById('contact').classList.remove('block');
+};
 
-export { displayNew };
+export default displayNew;
